145f24b971806f5232a7721d07e00f3f82ac4a9b
Eric Fernandes Martins <eric.fmartins11@gmail.com>
2025-09-13 09:01:51 -0300

    Adição de try except

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
+while True:
+    try:
+        nome = input("Digite o Nome: ")
+        validacao = bool(nome.replace(" ", "").isalpha() == True)
+    except:
+        print("o Nome não é valido")
+        break
+
+    try: 
+        salario = int(input("Digite o Salário: "))
+    except ValueError:
+        print("O salario não é um salario válido")
+        break
+    
+    try:
+        porcentagem = int(input("Qual a porcentagem do bonus: "))
+    except ValueError:
+        print("A porcentagem não é válida")
+        break
+    bonus = salario * porcentagem / 100
 
 
-nome = input("Digite o Nome: ")
-
-salario = int(input("Digite o Salário: "))
-
-porcentagem = int(input("Qual a porcentagem do bonus: "))
-
-bonus = salario * porcentagem / 100
-
-print(f"O bonus de {nome} será R${bonus}")
+    print(f"O bonus de {nome} será R${bonus}")
