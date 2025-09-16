1497cca84d1c5e8d26c80fa515b3da46fb15fda7
Eric Fernandes Martins <eric.fmartins11@gmail.com>
2025-09-16 06:51:48 -0300

    Adição de validação do nome, com loop para quando digitar numeros

--- main.py
+++ main.py
@@ -1,22 +1,32 @@
-while True:
+nome_valido = False
+salario_valido = False
+porcentagem_valida = False
+
+
+while not nome_valido:
     try:
         nome = input("Digite o Nome: ")
         validacao = bool(nome.replace(" ", "").isalpha() == True)
+        if validacao is True:
+            nome_valido = True
+        else:
+            print("Digite um nome  valido que contenha só letras")
     except:
         print("o Nome não é valido")
         break
-
+while not salario_valido:
     try: 
         salario = int(input("Digite o Salário: "))
     except ValueError:
         print("O salario não é um salario válido")
         break
-    
+while not porcentagem_valida:  
     try:
         porcentagem = int(input("Qual a porcentagem do bonus: "))
     except ValueError:
         print("A porcentagem não é válida")
         break
+    
     bonus = salario * porcentagem / 100
 
 
